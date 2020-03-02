61eb826ab49bb18d0e32575a675258c7bee2c04b
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-03-02 20:57:41 +0300

    init: add  functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+    var digit = 0;
+    var result = 0;
+
+    while( n ){
+        digit = n % 10                  
+        result = (result * 10) + digit  
+        n = n/10|0                      
+    }  
   
+    return result  
 }
