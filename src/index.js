4e57917bef22b278f603066297dc6e9ee64ea0cc
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-03-06 14:16:49 +0300

    init: update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-function reverseInt(n) {
-  const reversed = n.toString().split('').reverse().join(''); 
-  return Math.sign(n) * parseInt(reversed);
+function reverseInt(n){
+    n = n + "";
+    n.split("").reverse().join("");
+    return n
 }
