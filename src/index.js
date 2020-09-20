1b9d0056633ee43dc5421b4ab6f779b75d5e38af
paxom4ik4 <pasha.zelenko001@gmail.com>
2020-09-20 18:04:30 +0300

    feat: reverse-int task done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-module.exports = function reverse(n) {
+module.exports = reverse = (n) => {
     n = Math.sqrt(Math.pow(n, 2));
-    var res = +(n + '').split('').reverse().join('');
+    const res = +(n + "").split("").reverse().join("");
     return res;
-}
+};
